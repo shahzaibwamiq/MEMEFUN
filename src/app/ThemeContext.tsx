a74c437332f8e101832d6cb7424c3f8b5bb4e38a
yamaankhan20 <khanyamaan1@gmail.com>
2025-03-11 22:45:36 +0500

    Images issue resolved

--- src/app/ThemeContext.tsx
+++ src/app/ThemeContext.tsx
@@ -22,23 +22,28 @@ const ThemeContext = createContext<ThemeContextType>(
 export function ThemeProvider({ children }:ThemeProviderProps) {
     const [theme, setTheme] = useState("dark");
 
-    useEffect(() => {
-        const storedTheme = localStorage.getItem("theme");
-        if (storedTheme) {
-            setTheme(storedTheme);
-            document.body.classList.add(storedTheme);
-            document.body.setAttribute("data-bs-theme", storedTheme);
-        }
-    }, []);
-    const toggleTheme = () => {
-        const newTheme = theme === "dark" ? "light" : "dark";
-        setTheme(newTheme);
-        localStorage.setItem("theme", newTheme);
 
-        document.body.classList.remove(theme);
-        document.body.classList.add(newTheme);
-        document.body.setAttribute("data-bs-theme", newTheme);
-    };
+        useEffect(() => {
+            if (typeof window !== "undefined") {
+                const storedTheme = localStorage.getItem("theme");
+                if (storedTheme) {
+                    setTheme(storedTheme);
+                    document.body.classList.add(storedTheme);
+                    document.body.setAttribute("data-bs-theme", storedTheme);
+                }
+            }
+        }, []);
+        const toggleTheme = () => {
+            if (typeof window !== "undefined") {
+                const newTheme = theme === "dark" ? "light" : "dark";
+                setTheme(newTheme);
+                localStorage.setItem("theme", newTheme);
+
+                document.body.classList.remove(theme);
+                document.body.classList.add(newTheme);
+                document.body.setAttribute("data-bs-theme", newTheme);
+            }
+        };
 
     return (
         <ThemeContext.Provider value={{ theme, toggleTheme }}>
