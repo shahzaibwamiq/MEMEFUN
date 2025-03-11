6ec9e7d4aa96f1961d83a6c0f693aa6c31b090b6
yamaankhan20 <khanyamaan1@gmail.com>
2025-03-11 22:41:53 +0500

    Images issue resolved

--- src/app/ThemeContext.tsx
+++ src/app/ThemeContext.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import {createContext, useContext, useEffect, useState} from "react";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 interface ThemeContextType {
     theme: string;
